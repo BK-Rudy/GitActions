b99db02c91b81603b1b95be1d7b315560b1c3c35
brunorudy1 <brunorudy1@gmail.com>
2024-09-27 23:59:29 -0300

    Primeiro commit

---
+++ src/main/java/com/infnet/gitactions/controller/GitActionsController.java
@@ -0,0 +1,13 @@
+package com.infnet.gitactions.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitActionsController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Olá mundo!";
+    }
+}
